bce7fc05814338d3c0627de0607b7f6874f1d6c2
ymc023 <ymc023@163.com>
2016-03-07 16:15:49 +0800

    vimIDE

---
+++ README.md
@@ -0,0 +1,16 @@
+# vimIDE
+
+
+### 是一个把vim扩展成IDE的脚本
+###### 脚本参考自ma6174
+
+### 简易安装方法：
+
+ 1. 安装vim (你得先有vim) 
+ 2. 安装git
+ 3. clone配置文件： git clone git://github.com/ymc023/vimIDE
+ 4. cd vimIDE && bash vimIDEsetup.sh
+ 5. 等待脚本自动安装插件完成!
+
+
+
---
+++ vimIDEsetup.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+#Author:ymc023
+#Mail:ymc023@163.com 
+#Platform:centos7&debian8
+#Date:2016年02月04日 星期四 10时21分07秒
+
+echo "vimIDE setup is running now......"
+#apt
+if which apt-get >/dev/null; then
+    	sudo apt-get install -y vim vim-gnome ctags xclip astyle python-setuptools python-dev git
+#yum
+elif which yum >/dev/null; then
+	sudo yum install -y gcc vim git ctags xclip astyle python-setuptools python-devel	
+fi
+
+#Mac OS
+if which brew >/dev/null;then
+    echo "You are using HomeBrew tool"
+    brew install vim ctags git astyle
+fi
+sudo easy_install -ZU autopep8 
+sudo ln -s /usr/bin/ctags /usr/local/bin/ctags
+mv -f ~/.vimrc ~/.vimrc_old
+rm -rf ~/.vim/bundle/vundle
+echo  "install bundle..."
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+
+
+cp vimrc ~/.vimrc
+cp -rf after/ ~/.vim/
+cp -rf bundle/ ~/.vim/
+cp -rf dict/ ~/.vim/
+cp -rf ftdetect/ ~/.vim/
+cp -rf indent/ ~/.vim/
+cp -rf plugin/ ~/.vim/
+cp -rf syntax/ ~/.vim/
+cp -rf autoload/ ~/.vim/
+cp -rf colors/ ~/.vim/
+cp -rf doc/ ~/.vim/
+cp -rf ftplugin/ ~/.vim/
+cp -rf snippets/ ~/.vim/
+cp -rf markdown.pl/ ~/.vim/
+cp -rf tips.md/ ~/.vim/
+
+echo  "install plugin..."
+echo  "Wait for minutes please...and YouCompleteMe have a error. don't worry,just press enter!" >setuplog
+echo  "请稍候，在安装YouCompleteMe时会产一个错误，请忽略。直接按回车键即可">>setuplog
+vim  setuplog -c "PluginInstall" -c "q" -c "q" 
+rm -rf setuplog
+cd ~/.vim/bundle/YouCompleteMe
+./install.sh 
+
+echo "Congratulations!"
+
